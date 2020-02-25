322ece601e1e857bdc95490140ee1f7a54f7303a
EC2 Default User <ec2-user@ip-172-31-46-253.us-east-2.compute.internal>
2020-02-25 00:47:12 +0000

    module 3 skeleton

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'book#color'
+  get 'book/color'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
